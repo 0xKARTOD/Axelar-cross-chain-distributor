03442ced3a32c7e36f1553f0fea6128186576340
0xKARTOD <ZinovyevR2000@gmail.com>
2022-10-13 20:05:37 +0300

    Distributor v1.0

--- pages/index.tsx
+++ pages/index.tsx
@@ -104,24 +104,22 @@ const Home: NextPage = () => {
   return (
     <div>
       <div className="con row-span-1 card text-white">
+        <figure
+          className="center-text-picture"
+        />
         <h1 className="text-4xl text-white font-bold text-center">
           Cross Chain<br></br> 
-          Distributor & DAO voter
+          <span className="main-text">Distributor & DAO voter</span>
         </h1>
-        <h2 className="text-white text-center">Built by Impossible JoJo team</h2>
+        <h2 className="text-white text-center">Built by Impossible <span className="jojotext">JoJo</span> team</h2>
           
         <div className="card-body mx-auto">
-          <div className="center-text-black-border">
-            To get the total voting power for a particular campaign,<br></br>
-            select chain from the list below and aslo write the campaign ID.<br></br>
-             And press "SELECT".<br></br>
-          </div>
-          <figure
-            className="center-text-picture"
-          />
           <h2 className="card-title mx-auto">
-            Select a campaign ID: 
+            Total voting power:
           </h2>
+          <div className="center-text-vp">
+            {totalVPbalances} MVOTE
+          </div>
           <div className="form-control">
             <form
               className="flex flex-col w-full"
@@ -151,104 +149,17 @@ const Home: NextPage = () => {
                     })}
                       type="submit"
                   >
-                    SELECT
+                    SUBMIT
                   </button>
                 </div>
               </div>
             </form>
           </div>
-          <h2 className="card-title mx-auto">
-            Total voting power: {totalVPbalances} MVOTE.
-          </h2>
         </div>
-      </div>
-
-      <div className="col">
-        <div className="gap-20 mt-10 justify-items-center ">
-          {/* Voter part */}
-          <div className="row-span-2 shadow-xl card w-100 bg-base-100">
-            <div className="card-body">
-              <h1 className="text-4xl font-medium text-center">
-                Cross Chain DAO voter
-              </h1>
-            </div>
-            <div className="card-body">
-              <p className="center-text">
-                In this part you can vote for a certain proposal.
-              </p>
-              <p>
-                Steps for voting:<br></br>
-                - Input the specific campaign ID and amount at the input line<br></br>
-                - Select Chain<br></br>
-                - Press "VOTE"<br></br>
-              </p>
-              <h2 className="card-title">Connected wallet (Voter):</h2>
-
-                <p>
-                  Voter ({truncatedAddress(wallet.address)}) balance:{" "} 
-                  {senderBalance} MVOTE
-                  {}
-                </p>
-                <div className="justify-end mt-2 card-actions">
-                  <form
-                    className="flex flex-col w-full"
-                    onSubmit={handleOnSubmitVote}
-                  >
-                    <div>
-                      <label className="label">
-                        <span className="label-text">Proposal Information:</span>
-                      </label>
-                      <div className="w-full input-group">
-                        <input
-                          disabled={loading}
-                          required
-                          name="Voteamount"
-                          type="number"
-                          placeholder="Enter amount"
-                          className="w-full input input-bordered"
-                        />
-                        <input
-                          disabled={loading}
-                          required
-                          name="VoteID"
-                          type="number"
-                          placeholder="Enter Campaign"
-                          className="w-full input input-bordered"
-                        />
-                        <select disabled={loading} required className="select_list" name="DestinationChainVote">
-                          <option value="binance">Binance</option>
-                          <option value="avalanche">Avalanche</option>
-                        </select>
-                        <button
-                          className={cn("btn btn-primary", {
-                            loading,
-                            "opacity-30":
-                            loading,
-                            "opacity-100":
-                            !loading,
-                          })}
-                            type="submit"
-                        >
-                          VOTE
-                        </button>
-                      </div>
-                    </div>
-                    {VoteTxHash && (
-                        <a
-                          href={`https://testnet.axelarscan.io/gmp/${VoteTxHash}`}
-                          className="mt-2 link link-accent"
-                          target="blank"
-                        >
-                          Track your vote at axelarscan
-                        </a>
-                    )}
-                  </form>
-                </div>
-            </div>
-          </div>
+        <div className="center-text-mini">
+        To get the total voting power for a particular campaign, please enter campaign ID and select Chain
         </div>
-      </div>
-          
+      </div>     
 
 
       <div className="col">
@@ -257,26 +168,27 @@ const Home: NextPage = () => {
           <div className="row-span-2 shadow-xl card w-100 bg-base-100">
             <div className="card-body">
               <h1 className="text-4xl font-medium text-center">
-                Cross Chain Distributor
+              <span className="main-text-cross-chain">Cross Chain</span>
+              <br></br>Distributor
               </h1>
             </div>
             <div className="card-body">
-              <p className="center-text">
+              <div className="center-text">
                 Before you start voting, you need to distribute tokens to users.
-              </p>
-              <p>
-                In this case you need to: <br></br>
-                - Input the specific address, chains and amounts below <br></br>
-                - After each entry, press "ADD" <br></br>
-                - And click on "DISTRIBUTE TOKENS" <br></br>
-              </p>
+              </div>
+              In this case you need to: <br></br>
+              <ol className="olli">
+                <li>Input the specific address, chains and amounts below</li>
+                <li>After each entry, press "ADD"</li>
+                <li>And click on "DISTRIBUTE TOKENS"</li>
+              </ol>
               <h2 className="card-title">Connected wallet (Distributor):</h2>
 
-              <p>
+              <div>
                 Sender ({truncatedAddress(wallet.address)}) balance:{" "} 
                 {senderBalance} MVOTE
                 {}
-              </p>
+              </div>
 
               <label className="label">
                 <h2 className="card-title">Recepients:</h2>
@@ -364,7 +276,7 @@ const Home: NextPage = () => {
                     onSubmit={handleOnSubmitDistribute}
                     >
                     <button
-                      className={cn("btn btn-primary", {
+                      className={cn("btn gradient-button", {
                         loading,
                         "opacity-30":
                         loading,
@@ -383,10 +295,99 @@ const Home: NextPage = () => {
         </div>
       </div>
 
+
+      <div className="col">
+        <div className="gap-20 mt-10 justify-items-center ">
+          {/* Voter part */}
+          <div className="row-span-2 shadow-xl card w-100 bg-base-100">
+            <div className="card-body">
+              <h1 className="text-4xl font-medium text-center">
+                <span className="main-text-cross-chain">Cross Chain</span>
+                <br></br>DAO Voter
+              </h1>
+            </div>
+            <div className="card-body">
+              <div className="center-text">
+                In this part you can vote for a certain proposal.
+              </div>
+              <div>
+                Steps for voting:<br></br>
+                <ol className="olli">
+                  <li>Input the specific campaign ID and amount at the input line. Select Chain</li>
+                  <li>Press "VOTE"</li>
+                </ol>
+              </div>
+              <h2 className="card-title">Connected wallet (Voter):</h2>
+
+                <div>
+                  Voter ({truncatedAddress(wallet.address)}) balance:{" "} 
+                  {senderBalance} MVOTE
+                  {}
+                </div>
+                <div className="justify-end mt-2 card-actions">
+                  <form
+                    className="flex flex-col w-full"
+                    onSubmit={handleOnSubmitVote}
+                  >
+                    <div>
+                      <label className="label">
+                        <span className="label-text">Proposal Information:</span>
+                      </label>
+                      <div className="w-full input-group">
+                        <input
+                          disabled={loading}
+                          required
+                          name="Voteamount"
+                          type="number"
+                          placeholder="Enter amount"
+                          className="w-full input input-bordered"
+                        />
+                        <input
+                          disabled={loading}
+                          required
+                          name="VoteID"
+                          type="number"
+                          placeholder="Enter Campaign"
+                          className="w-full input input-bordered"
+                        />
+                        <select disabled={loading} required className="select_list" name="DestinationChainVote">
+                          <option value="binance">Binance</option>
+                          <option value="avalanche">Avalanche</option>
+                        </select>
+                      </div>
+                    </div>
+                    <br></br>
+                    <button
+                      className={cn("btn gradient-button", {
+                        loading,
+                        "opacity-30":
+                        loading,
+                        "opacity-100":
+                        !loading,
+                      })}
+                        type="submit"
+                    >
+                      VOTE
+                    </button>
+                    {VoteTxHash && (
+                        <a
+                          href={`https://testnet.axelarscan.io/gmp/${VoteTxHash}`}
+                          className="mt-2 link link-accent"
+                          target="blank"
+                        >
+                          Track your vote at axelarscan
+                        </a>
+                    )}
+                  </form>
+                </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      
       <p>
         -
       </p>
-      
     </div>
   );
 };
